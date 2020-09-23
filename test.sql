6403e7fa0e9dbeb3936ab588d3314c9972bd71c7
madhuripenikalapti <37667438+madhuripenikalapti@users.noreply.github.com>
2020-09-23 18:21:27 +0530

    Update test.sql

--- test.sql
+++ test.sql
@@ -5,3 +5,15 @@ ROW FORMAT DELIMITED \
 FIELDS TERMINATED BY ‘\t’ \
 LINES TERMINATED BY ‘\n’ \
 STORED AS TEXTFILE;
+create table test (col1 varchar(100), col2 char(100), col3 int, col4 int)
+partitioned by ( col1 varchar(100), col2 char(100), col4 int);
+ALTER TABLE employee CHANGE name ename String;
+ALTER TABLE foo PARTITION (ds='2008-04-08', hr=11) CHANGE COLUMN dec_column_name dec_column_name DECIMAL(38,18)
+ALTER TABLE test_change CHANGE a a1 INT;
+ALTER TABLE test_change CHANGE a1 a2 STRING AFTER b;
+ALTER TABLE test_change CHANGE c c1 INT FIRST;
+ALTER TABLE testchange CHANGE a a INT COMMENT 'this is column a1';
+
+ALTER TABLE test ADD COLUMNS ( a STRING COMMENT 'First column', b varchar(100));
+
+ALTER TABLE test REPLACE COLUMNS ( a INT a1 Int, b STRING b1 String, c Varchar(100) c1 varchar(100));
