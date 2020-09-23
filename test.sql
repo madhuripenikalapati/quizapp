71c36ae477c81588e21c16d876a3170f19b4e406
Madhuri Penikalapati <madhuri.penikalapati@knowledgelens.com>
2020-09-23 16:43:36 +0530

    1

--- test.sql
+++ test.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS employee ( eid int, name string, \
-salary String, destination String) \
+salary String, destination varchar(100)) \
 COMMENT ‘Employee details’ \
 ROW FORMAT DELIMITED \
 FIELDS TERMINATED BY ‘\t’ \
