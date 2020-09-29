33bcab09b425f52dcb3eed16c802f61ab4ebfa31
madhuripenikalapti <37667438+madhuripenikalapti@users.noreply.github.com>
2020-09-29 19:49:46 +0530

    Update test.sql

--- test.sql
+++ test.sql
@@ -5,3 +5,9 @@ ROW FORMAT DELIMITED \
 FIELDS TERMINATED BY ‘\t’ \
 LINES TERMINATED BY ‘\n’ \
 STORED AS TEXTFILE;              
+ALTER TABLE employee CHANGE name ename String;
+ALTER TABLE foo PARTITION (ds='2008-04-08', hr=11) CHANGE COLUMN dec_column_name dec_column_name DECIMAL(38,18)
+ALTER TABLE test_change CHANGE a a1 INT;
+ALTER TABLE test_change CHANGE a1 a2 STRING AFTER b;
+ALTER TABLE test_change CHANGE c c1 INT FIRST;
+ALTER TABLE testchange CHANGE a a INT COMMENT 'this is column a1';
